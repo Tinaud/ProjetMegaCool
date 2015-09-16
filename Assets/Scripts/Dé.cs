62671f145cca3748c9a9cf3eaac95dd8f5ae3ed2
Mathieu <mathieu.naud@hotmail.com>
2015-09-15 21:21:11 -0300

    cube qui vole

---
+++ Assets/Scripts/Dé.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dé : MonoBehaviour 
+{
+    public float thrust = 5f;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(transform.right * thrust);
+    }
+
+    void FixedUpdate()
+    {
+            
+    }
+}

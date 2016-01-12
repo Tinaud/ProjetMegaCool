537224cd4ee4c4350acf623243ca9d844d0ce330
unknown <samuellalibel@hotmail.com>
2016-01-12 18:19:04 -0500

    no message

--- Assets/Scripts/MenuPlayers.cs
+++ Assets/Scripts/MenuPlayers.cs
@@ -3,21 +3,26 @@ using System.Collections;
 
 public class MenuPlayers : MonoBehaviour {
 
-	public int NbPlayer = 0;
-	public int NbTurns = 0;
+	public int NbPlayer;
+	public int NbTurns;
+	public GameObject play;
 
 	// Use this for initialization
 	void Start () {
-	
+		NbPlayer = 0;
+		NbTurns = 0	;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (NbPlayer != 0 && NbTurns != 0)
+			play.SetActive(true);
+			
 	}
 
 	public void player2 ()	{
 		NbPlayer = 2;
+		Debug.Log("2 players");
 	}
 
 	public void player3 ()	{

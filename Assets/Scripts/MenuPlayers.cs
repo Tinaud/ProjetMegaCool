44c80325975f5480404eec0c31ca39337ecbf71f
unknown <samuellalibel@hotmail.com>
2016-01-12 17:40:35 -0500

    no message

---
+++ Assets/Scripts/MenuPlayers.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuPlayers : MonoBehaviour {
+
+	public int NbPlayer = 0;
+	public int NbTurns = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void player2 ()	{
+		NbPlayer = 2;
+	}
+
+	public void player3 ()	{
+		NbPlayer = 3;
+	}
+
+	public void player4 ()	{
+		NbPlayer = 4;
+	}
+
+	public void turns10 ()	{
+		NbTurns = 10;
+	}
+
+	public void turns15 ()	{
+		NbTurns = 15;
+	}
+
+	public void turns20 ()	{
+		NbTurns = 20;
+	}
+}

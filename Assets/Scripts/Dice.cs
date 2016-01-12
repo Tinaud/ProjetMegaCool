e26ca4c10896fbcddcabeaa28ee61fd5d43fecc2
Mathieu <mathieu.naud@hotmail.com>
2016-01-12 17:36:49 -0500

    Dice v2.0 + GameManager Start + Event Thing Start

---
+++ Assets/Scripts/Dice.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dice : MonoBehaviour 
+{
+    private Transform dice;
+    private bool isSpinning;
+    private int patate;  //Random Number
+
+    void Start()
+    {
+        dice = GetComponent<Transform>();
+        isSpinning = false;
+    }
+
+    void Update()
+    {
+        if (isSpinning)
+            dice.Rotate(20 * Random.value, 20 * Random.value, 20 * Random.value);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isSpinning)
+                StopDice();
+            else
+                ThrowDice();
+        }
+    }
+
+    IEnumerator MyCoroutine()
+    {
+        patate = Random.Range(1, 7);
+
+        switch (patate)
+        {
+            case 2:
+                Debug.Log("2");
+                dice.localEulerAngles = new Vector3(Random.Range(-10, 10), Random.Range(80, 100), Random.Range(170, 190));
+                break;
+            case 3:
+                Debug.Log("3");
+                dice.localEulerAngles = new Vector3(Random.Range(170, 190), Random.Range(-10, 10), Random.Range(-10, 10));
+                break;
+            case 4:
+                Debug.Log("4");
+                dice.localEulerAngles = new Vector3(Random.Range(-10, 10), Random.Range(-10, 10), Random.Range(-10, 10));
+                break;
+            case 5:
+                Debug.Log("5");
+                dice.localEulerAngles = new Vector3(Random.Range(-10, 10), Random.Range(265, 280), Random.Range(170, 190));
+                break;
+            case 6:
+                Debug.Log("6");
+                dice.localEulerAngles = new Vector3(Random.Range(80, 100), Random.Range(-10, 10), Random.Range(-10, 10));
+                break;
+            default:
+                Debug.Log("1");
+                dice.localEulerAngles = new Vector3(Random.Range(265, 280), Random.Range(-10, 10), Random.Range(-10, 10));
+                break;
+        }
+
+        yield return new WaitForSeconds(2f);
+        Destroy(this.gameObject);
+    }
+
+    public void ThrowDice()
+    {
+        isSpinning = true;
+    }
+
+    public void StopDice()
+    {
+        isSpinning = false;
+        StartCoroutine(MyCoroutine());
+    }
+}
---
+++ Assets/Scripts/Events.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Events : MonoBehaviour 
+{
+    public string[,] allEvents = new string[17, 3] { 
+        { "Scientists want to help endangered species! The player that pocess the most dinosaur loses his most expensive (only one). If two or more players are equals, every of them must do this event. Ignore this event if no one have dinosaur.", "Endangered species", "1"}, 
+		{ "Every player receive 10$!" , "Pay day!", "3"},
+		{ "There are power outage in the parks, every player raise his danger level by 1 for this turn only.", "Power outage", "3"}, 
+		{ "New tranquilizing weapons are in sale! Every player drop his danger level by 1 for this turn only.", "Prevention program", "3"},
+		{ "Every player throw 1d6 and that number is the number of new guests they get in their park.", "advertisement on cereal boxes", "2"},
+		{ "Dinosaurs enter in their mating season! You must do two breaches this turn. If you get a 1 on both die, you suffer the effects of two breaches. Ignore this event if you have a paleontologist booth in your park.", "Mating season", "5"},
+		{ "5 new guests visit your park!", "Spring Break!", "3"},
+		{ "The government loves you! You can have two actions this turn for the building phase. You only have to do the breach once.", "Tax return", "5"},	
+		{ "Your dinosaurs are making a roar contest and your guests are afraid! Everyone lose 5 guests in their park. Ignore this event if you have a paleontologist booth in your park.", "King of the jungle", "2"},
+		{ "Your dinosaurs will have new born soon! You can buy a second dinosaur only for this turn if you already have that same dinosaur in your park.", "Proliferation", "5"},
+        { "", "The mysterious men in white", "1"},
+        { "", "Know-it-all Joe", "1"},
+        { "Some school buses are visiting your park! You gain 5 new guests and you receive 5$.", "Summer camp", "3"},
+        { "", "Accumulation of defecation", "3"},
+        { "You can build as many cages as you want during this turn, as long as you have the money and space in your park! You can then continue the normal building phase by buying something else.", "Metals abundance", "6"},
+        { "Spectacular discoveries have happened in the archaeological world. Triceratops's price is reduced by 5$ only for this turn.", "Archaeological research : Triceratops", "3"},
+        { "Spectacular discoveries have happened in the archaeological world. Tyrannosaurus's price is reduced by 5$ only for this turn.", "Archaeological research : Tyrannosaurus", "3"}};
+
+	void Start () 
+    {
+	
+	}
+	
+	void Update () 
+    {
+	
+	}
+}

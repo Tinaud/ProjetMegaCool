a34ac857199490b32c72c3bfdd7b7b1403b0ff02
BarbeBleue <simoncoteduchesne@gmail.com>
2016-01-14 21:29:47 -0500

    Parc manager

---
+++ Assets/Scripts/ParcManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParcManager : MonoBehaviour {
+    public int cage;
+    public int cash;
+    public int visitors;
+    
+    // Use this for initialization
+    void Start () {
+
+        int[] dinos = new int[] { 0, 0, 0, 0 };
+        int[] booth = new int[] { 0, 0, 0, 0, 0, 0 };
+        cage = 1;
+        cash = 15;
+        visitors = 0;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

c1640c54e9db8076eabc38c1f7cff50008bd2455
unknown <samuellalibel@hotmail.com>
2016-01-19 13:53:34 -0500

    mise a jour

--- Assets/Scripts/KiosksSprite.cs
+++ Assets/Scripts/KiosksSprite.cs
@@ -6,10 +6,13 @@ public class KiosksSprite : MonoBehaviour {
 
     public Canvas spri;
     public Sprite pal;
+    public int width;
+    public int height;
 
 	// Use this for initialization
 	void Start () {
         spri.GetComponentInChildren<Image>().sprite = pal;
+        spri.GetComponentInChildren<Image>().rectTransform.localScale = new Vector3(2,0,2);
         Debug.Log("essai");
 	}
 	

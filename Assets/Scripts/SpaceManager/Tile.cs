c5509a5ebe6fcd7e7f45e92a73c917c91230854e
gchaville <gillian.chaville@gmail.com>
2016-01-30 17:12:31 -0500

    Avancement purchase.

--- Assets/Scripts/SpaceManager/Tile.cs
+++ Assets/Scripts/SpaceManager/Tile.cs
@@ -3,21 +3,40 @@ using System.Collections;
 
 public class Tile : MonoBehaviour {
 
-	public bool isActive;
 	bool isAvailable;
+	bool isSelected;
+	Vector2 position;
 
-	//public enum ObjType {Cage, Dinosaur, Booth, Other};
-	//ObjType type;
+	public Vector2 Position {
+		get { return position; }
+		set { position = value; }
+	}
+
+	public enum ObjType {
+		Empty, 
+		CageEmpty, 
+		CageVelo, 
+		CageTyra, 
+		CageBront,
+		CageTric,
+		Restaurant, 
+		Security,
+		Bathroom, 
+		Casino, 
+		Spy, 
+		Paleontologist
+	};
+	ObjType type;
+
+	public ObjType Type {
+		get { return type; }
+		set { type = value; }
+	}
 
 	/*----------------- GETTER & SETTER ------------------*/
 
 	public bool IsAvailable {
 		get { return isAvailable; }
-	}
-
-	public bool IsActive {
-		get { return isActive; }
-		set {isActive = value;}
 	}
 
 	public void SetAvailable() {
@@ -30,23 +49,28 @@ public class Tile : MonoBehaviour {
 		
 
 	void Start () {
+		isSelected = false;
+		SetAvailable ();
+		type = ObjType.Empty;
 	}
 
 	void Update () {
 
-		/* Pour la selection des tuiles
-		 * if (GetComponent<Renderer>().isVisible && Input.GetMouseButtonUp (0)) {
+		 //Pour la selection des tuiles
+		if (GetComponent<Renderer> ().isVisible) {
 			Vector3 camPos = Camera.main.WorldToScreenPoint (transform.position);
 			camPos.y = CameraOperator.InvertMouseY (camPos.y);
-			IsSelected = CameraOperator.selection.Contains (camPos);
+			isSelected = CameraOperator.selection.Contains (camPos);
 		}
-		*/
-
-		if (isActive)
-			GetComponent<Renderer>().material.color = Color.blue;
+			
+		if (isSelected) {
+			if (isAvailable)
+				GetComponent<Renderer> ().material.color = Color.green;
+			else
+				GetComponent<Renderer> ().material.color = Color.red;
+		}
 		else
-			GetComponent<Renderer>().material.color = Color.red;
-
+			GetComponent<Renderer> ().material.color = Color.white;
 	}
 }
 
--- Assets/Scripts/Unused Scripts/CameraOperator.cs
+++ Assets/Scripts/Unused Scripts/CameraOperator.cs
@@ -4,35 +4,31 @@ using System.Collections;
 public class CameraOperator : MonoBehaviour {
 	public Texture2D selectionHighLight = null;
 	public static Rect selection = new Rect(0, 0, 0, 0);
-	Vector3 startClick = -Vector3.one;
-
+	static float x = 810, y = 323;
+	Vector2 startPos =  new Vector2(x, InvertMouseY(y));
+	Vector2 width = new Vector2(23, 23);
 
 	// Use this for initialization
 	void CheckCamera () {
-		if (Input.GetMouseButtonDown (0))
-			startClick = Input.mousePosition;
-		else if (Input.GetMouseButtonUp (0)) {
-			if (selection.width < 0) {
-				selection.x += selection.width;
-				selection.width = -selection.width;
-			}
-			if (selection.height < 0) {
-				selection.y += selection.height;
-				selection.height = -selection.height;
-			}
+		if(Input.GetKeyDown(KeyCode.UpArrow))
+			y += 22; 
 
-			startClick = -Vector3.one;
-		}
-		if (Input.GetMouseButton(0)) 
-			selection = new Rect(startClick.x, InvertMouseY(startClick.y), Input.mousePosition.x - startClick.x,
-				InvertMouseY(Input.mousePosition.y) - InvertMouseY(startClick.y));
+		if(Input.GetKeyDown(KeyCode.DownArrow))
+			y -= 22; 
+
+		if(Input.GetKeyDown(KeyCode.RightArrow))
+			x += 22; 
+
+		if(Input.GetKeyDown(KeyCode.LeftArrow))
+			x -= 22;
+			
+		startPos =  new Vector2(x, InvertMouseY(y));
+		selection = new Rect(startPos, width);
 	}
 
 	void OnGUI () {
-		if (startClick != -Vector3.one) {
 			GUI.color = new Color (1, 1, 1, 0.5f);
 			GUI.DrawTexture (selection, selectionHighLight);
-		}
 	}
 
 	public static float InvertMouseY(float y) {

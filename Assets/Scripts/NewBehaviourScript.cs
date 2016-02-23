b0b76bfc641899f87bd18c540679e7efc826c2c3
Mathieu Naud <mathieu.naud@hotmail.com>
2016-02-23 18:13:32 -0500

    Version upgrade et gros travail sur les dés! :D

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -3,28 +3,34 @@ using System.Collections;
 
 public class NewBehaviourScript : MonoBehaviour 
 {
-    private bool isSpinning;
+    public bool isMoving,
+                 isSpinning;
     private Rigidbody rb;
 
-	void Start () 
+	void Awake () 
     {
+        isMoving = true;
         isSpinning = true;
         rb = GetComponent<Rigidbody>();
         transform.Rotate(Random.Range(-200, 200), Random.Range(-200, 200), Random.Range(-200, 200));
-        //rb.AddForce(Random.Range(200, 500), 100, Random.Range(200, 500));
-        rb.AddForce(Random.Range(-300, 300), 300, 500);
+        rb.AddForce(Random.Range(-300, 300), 200, Random.Range(200, 500));
         StartCoroutine(ThrowDice());
 	}
 
 	void Update () 
     {
-        if (isSpinning)
+        if(isSpinning)
             transform.Rotate(9 * Random.value, 9 * Random.value, 9 * Random.value);
+        if (rb.IsSleeping())
+            isMoving = false;
 	}
 
     IEnumerator ThrowDice()
     {
         yield return new WaitForSeconds(0.5f);
         isSpinning = false;
+        yield return new WaitWhile(() => isMoving);
+        yield return new WaitForSeconds(0.5f);
+        Destroy(this.transform.parent.gameObject);
     }
 }
